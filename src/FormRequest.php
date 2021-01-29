58cbce7fa7a9b0daafe762d15f3e334c3b143538
king east <1207877378@qq.com>
2021-01-29 09:33:00 +0800

    form request

---
+++ src/FormRequest.php
@@ -0,0 +1,228 @@
+<?php
+/**
+ * +----------------------------------------------------------------------
+ * | Name: keAdmin
+ * | Author King east To 1207877378@qq.com
+ * +----------------------------------------------------------------------
+ */
+
+
+namespace ke\utils;
+
+
+use think\App;
+use think\Container;
+use think\exception\ValidateException;
+
+class FormRequest
+{
+    /**
+     * @var App
+     */
+    protected $app;
+
+
+    protected $data;
+
+
+    private $validate;
+
+
+    protected $rules = [];
+
+
+    private $ext_keys = [];
+
+
+    private $fields = [];
+
+
+    private $alias = [];
+
+
+    public function __construct($rule, $scene = null)
+    {
+        if (defined('TP_VERSION') && TP_VERSION == '6.x') {
+            $this->app = Container::pull('app');
+            if (is_string($rule)) {
+                $n = new $rule;
+                $this->rules = $n->rule;
+                $this->rules = $this->ruleFilter($this->rules);
+                $this->validate = $this->app->validate;
+                $this->validate->rule($rule);
+            } else {
+                $this->rules = $rule;
+                $this->rules = $this->ruleFilter($this->rules);
+                $this->validate = $this->app->validate;
+                $this->validate->rule($this->rules);
+            }
+        } else {
+            $this->app = Container::get('app');
+            if (is_string($rule)) {
+                $n = new $rule;
+                $this->rules = $n->rule;
+                $this->rules = $this->ruleFilter($this->rules);
+                $this->validate = $this->app->validate($rule);
+            } else {
+                $this->rules = $rule;
+                $this->rules = $this->ruleFilter($this->rules);
+                $this->validate = $this->app->validate();
+                $this->validate->rule($this->rules);
+            }
+        }
+
+        $this->data = $this->app->request->param();
+        if (!is_null($scene)) {
+            $this->scene($scene);
+        }
+    }
+
+
+    private function ruleFilter($rule)
+    {
+        $arr = [];
+        foreach ($rule as $str=>$value) {
+            if (is_int($str)) {
+                if (strpos($value, '|') === false) {
+                    $this->fields[] = $value;
+                } else {
+                    [$l, $r] = explode('|', $value);
+                    $this->fields[] = $l;
+                }
+            } else {
+                if (strpos($str, '|') === false) {
+                    $this->fields[] = $str;
+                } else {
+                    $this->fields[] = explode('|', $str)[0];
+                }
+                $arr[$str] = $value;
+            }
+        }
+
+        return $arr;
+    }
+
+
+    /**
+     * 扩展可选无规则字段
+     * @param array $fields
+     * @return $this
+     */
+    public function extend(array $fields)
+    {
+        $this->fields = array_merge($this->fields, $fields);
+
+        return $this;
+    }
+
+
+    /**
+     * 设置验证场景
+     * @param string $name
+     * @return $this
+     */
+    public function scene($name)
+    {
+        $this->validate->scene($name);
+        return $this;
+    }
+
+
+    /**
+     * 设置提示信息
+     * @param array $message
+     * @return $this
+     */
+    public function message($message)
+    {
+        $this->validate->message($message);
+        return $this;
+    }
+
+
+    /**
+     * 验证
+     * @param array $data
+     * @return array
+     */
+    public function check($data = null)
+    {
+        if (is_null($data)) {
+            $this->data = $this->app->request->param();
+        } else {
+            $this->data = $data;
+        }
+        if ($this->validate->check($this->data)) {
+            return $this->toArray();
+        }
+        throw new ValidateException($this->validate->getError());
+    }
+
+
+    public function getError()
+    {
+        return $this->validate->getError();
+    }
+
+
+    /**
+     * 设置key的默认值,如果key不在rules列表里则不生效
+     * @param array $data
+     * @return $this
+     */
+    public function setDefault(array $data)
+    {
+        foreach ($data as $key=>$val) {
+            if (!isset($this->data[$key])) {
+                $this->data[$key] = $val;
+            }
+        }
+        return $this;
+    }
+
+
+    /**
+     * 设置键别名
+     * @param array $data
+     * @return $this
+     */
+    public function setAlias(array $data)
+    {
+        $this->alias = $data;
+        return $this;
+    }
+
+
+    /**
+     * @return array
+     */
+    public function toArray()
+    {
+        $postData = [];
+        foreach ($this->fields as $f) {
+            if (isset($this->data[$f])) {
+                $postData[$f] = $this->data[$f];
+            }
+        }
+        foreach ($this->ext_keys as $key) {
+            if (isset($this->data[$key])) {
+                $postData[$key] = $this->data[$key];
+            }
+        }
+        // 别名转换
+        foreach ($this->alias as $from=>$to) {
+            if (isset($postData[$from])) {
+                $postData[$to] = $postData[$from];
+                unset($postData[$from]);
+            }
+        }
+        return $postData;
+    }
+
+
+    public function has($name)
+    {
+        return isset($this->data[$name]);
+    }
+
+}
